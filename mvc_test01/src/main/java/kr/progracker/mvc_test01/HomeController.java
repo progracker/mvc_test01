c1b48f2953b814fc0db1a5a5429054902183c321
progracker <progracker@progracker-PC>
2016-08-07 16:03:13 +0900

    @Resource > @Autowired변경

--- mvc_test01/src/main/java/kr/progracker/mvc_test01/HomeController.java
+++ mvc_test01/src/main/java/kr/progracker/mvc_test01/HomeController.java
@@ -6,10 +6,9 @@ import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
 
-import javax.annotation.Resource;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -43,7 +42,7 @@ public class HomeController {
 		return "home";
 	}
 	
-	@Resource(name="userDaoImpl")
+	@Autowired
 	private UserDaoImpl userDaoImpl;
 	
 	@RequestMapping("/join.pro")
